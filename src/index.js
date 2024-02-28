3998a7f1754cfb3b0aa5ef7a3f670752d1ab3358
Djennad Rany <128905298+DjennadRany@users.noreply.github.com>
2024-02-28 20:27:36 +0100

    finalisationprojet-soutenance
    finalisationprojet-soutenance

--- src/index.js
+++ src/index.js
@@ -1,22 +1,78 @@
-import reportWebVitals from './reportWebVitals';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store/configureStore';
-import App from './App';
+import { configureStore } from "@reduxjs/toolkit";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import {
+  Outlet,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import Error from "./components/Error";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import "./index.css";
+import loginSlice from "./reducers/auth";
+import { Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AdminNavbar from "./components/AdminNavbar";
+import Home from "./components/Home";
+import "./App.css";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const store = configureStore({
+  reducer: {
+    login: loginSlice.reducer,
+  },
+});
 
+function BasicLayout() {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    return <Navigate push to="/profile" />;
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
+function AdminLayout() {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate push to="/login" />;
+  }
+
+  return (
+    <>
+      <AdminNavbar />
+      <Outlet />
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<BasicLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+          <Route path="/" element={<AdminLayout />}>
+            <Route index path="/profile" element={<Profile />} />
+          </Route>
+
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  </Provider>
+);
